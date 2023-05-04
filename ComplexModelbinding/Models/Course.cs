3fbe535ed65c59210970e2ccf362951d0c773f28
Cheyenne Edgar <cheyennerivers03@gmail.com>
2023-05-04 10:23:23 -0700

    Created a course model

---
+++ ComplexModelbinding/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComplexModelbinding.Models
+{
+    public class Course
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public Instructor Teacher { get; set; }
+    }
+}
